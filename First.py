47385d21d680c0b7eb4566cbbfa58018b66e9333
Rustem <ironrustem@gmail.com>
2021-03-16 23:35:18 +0300

    finish

--- First.py
+++ First.py
@@ -1,53 +1,60 @@
 import vk_api
 import psycopg2
-
-con = psycopg2.connect(host='database-course.cuh9nfz6hd2g.us-east-1.rds.amazonaws.com',
-                       port='5432', user='postgres', password='BoskaData')
-
-vk_session = vk_api.VkApi(token="ad3741635a3e27742a193267f4d82b753a16d0d61cb966cd3f103b77b1cff1c0ec12f3457b4278f520e60")
-vk = vk_session.get_api()
-
-walls1 = vk.wall.get(domain='itis_kfu', count=100)
-walls2 = vk.wall.get(domain='itis_kfu', count=100, offset=100)
+import sys
 
 
-def sortWall(walls, un_words1):
-    if not un_words1:
-        un_words1 = {}
-    un_words = un_words1
-    for wall in walls['items']:
-        words = wall['text'].split(' ')
-        for j in words:
-            j = j.strip()
-            words3 = j.split('\n')
-            for word in words3:
-                words3 = word.split(' ')
-                for i1 in words3:
-                    if i1 != "":
-                        marks = [')', '(', '\n', '•', ',', ';', '•', '*', '%', '₽', '$', '/', '-', '.', '!', '—', '\"',
-                                 '\'', '@',
-                                 '^', ':', '»', '«', '◾', '“']
-                        for i2 in marks:
-                            i1 = i1.strip(i2)
+def words(passwordDataBase, tokenVK):
+    con = psycopg2.connect(host='database-course.cuh9nfz6hd2g.us-east-1.rds.amazonaws.com',
+                           port='5432', user='postgres', password=passwordDataBase)
+
+    vk_session = vk_api.VkApi(token=tokenVK)
+    vk = vk_session.get_api()
+
+    walls1 = vk.wall.get(domain='itis_kfu', count=100)
+    walls2 = vk.wall.get(domain='itis_kfu', count=100, offset=100)
+
+    def sortWall(walls, un_words1):
+        if not un_words1:
+            un_words1 = {}
+        un_words = un_words1
+        for wall in walls['items']:
+            words = wall['text'].split(' ')
+            for j in words:
+                j = j.strip()
+                words3 = j.split('\n')
+                for word in words3:
+                    words3 = word.split(' ')
+                    for i1 in words3:
                         if i1 != "":
-                            if i1 in un_words.keys():
-                                un_words[str(i1)] = un_words.get(str(i1)) + 1
-                            else:
-                                un_words[str(i1)] = 1
-    return un_words
+                            marks = [')', '(', '\n', '•', ',', ';', '•', '*', '%', '₽', '$', '/', '-', '.', '!', '—',
+                                     '\"',
+                                     '\'', '@',
+                                     '^', ':', '»', '«', '◾', '“']
+                            for i2 in marks:
+                                i1 = i1.strip(i2)
+                            if i1 != "":
+                                if i1 in un_words.keys():
+                                    un_words[str(i1.lower())] = un_words.get(str(i1.lower())) + 1
+                                else:
+                                    un_words[str(i1.lower())] = 1
+        return un_words
 
+    un_wordsR = sortWall(walls1, {})
+    un_wordsR = sortWall(walls2, un_wordsR)
 
-un_wordsR = sortWall(walls1, {})
-un_wordsR = sortWall(walls2, un_wordsR)
+    leng = len(un_wordsR)
+    with con.cursor() as cursor:
+        i = 0
+        cursor.execute(f'''TRUNCATE TABLE  topwords;''')
+        for w in un_wordsR:
+            print(f'\r записывается {i + 1} из {leng}', end="", flush=True)
+            cursor.execute(f'''INSERT INTO topwords (word, count) VALUES('{w}',{un_wordsR[w]});''')
+            i += 1
 
-leng = len(un_wordsR)
-with con.cursor() as cursor:
-    i = 0
-    cursor.execute(f'''TRUNCATE TABLE  topwords;''')
-    for w in un_wordsR:
-        print(f'\r записывается {i + 1} из {leng}', end="", flush=True)
-        cursor.execute(f'''INSERT INTO topwords (word, count) VALUES('{w}',{un_wordsR[w]});''')
-        i += 1
+    con.commit()
+    con.close()
 
-con.commit()
-con.close()
+if len(sys.argv) == 3:
+    words(sys.argv[1], sys.argv[2])
+else:
+    print("Print with me passwordDataBase and tokenVK")

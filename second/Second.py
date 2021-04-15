83b8fef66c7d37dc83da3e88800ff3dac44214d5
Rustem <rustem@four.local>
2021-04-15 15:19:11 +0300

    try amazon

--- second/Second.py
+++ second/Second.py
@@ -1,176 +1,184 @@
 import urllib.request
-import urllib.parse
+from urllib.parse import urlparse
 import re
 import threading
 import logging
 from graphviz import Digraph
 
+ng = []
+
 
 def get_logger():
-    logger = logging.getLogger("threading_example")
-    logger.setLevel(logging.DEBUG)
+    logger1 = logging.getLogger("threading_example")
+    logger1.setLevel(logging.DEBUG)
     fh = logging.FileHandler("threading.log")
     fmt = '%(message)s'
     formatter = logging.Formatter(fmt)
     fh.setFormatter(formatter)
-    logger.addHandler(fh)
-    return logger
+    logger1.addHandler(fh)
+    return logger1
 
 
-def parseLinks(g, url, level):
-    print("start")
+# g-graph
+# url-this url
+# level-level deep
+# url1-past url, use for path in graph
+def parseLinks(ng1, g, urlSite, level, urlPast):
+    print("start: " + urlSite + "|")
     level += 1
-    values = {'s': 'pandas',
-              'submit': 'search'}
-    data = urllib.parse.urlencode(values)
-    data = data.encode('utf-8')
-    req = urllib.request.Request(url, data)
-    resp = urllib.request.urlopen(req)
-    respData = resp.read()
-    paragraphs = re.findall(r'<a href="(.*?)"', str(respData))
 
-    url1 = url.replace("https://", "")
-    url2 = str(url1.replace("http://", ""))
+    fp = urllib.request.urlopen("http://www.python.org")
+    mybytes = fp.read()
+    mystr = mybytes.decode("utf8")
+    fp.close()
+    paragraphs = re.findall(r'<a href="(.*?)"', str(mystr))
+
+    urlSite1 = urlSite.replace("https://", "")
+    urlSite1 = urlSite1.replace("http://", "")
+    ng.append(urlSite1)
 
     n = 0
+    threads = []
+    print("n1: " + str(len(paragraphs)))
     for i in range(len(paragraphs)):
-        h = str(paragraphs[i - 1])
-        if not h.startswith('#'):
-            if h.startswith('http'):
-                h1 = h.replace("https://", "")
-                h2 = str(h1.replace("http://", ""))
 
-                g.edge(url2, h2)
-                if level < 3:
-                    n += 1
-                    x = threading.Thread(target=parseLinks, args=(g, i, level,))
-                    x.start()
-                    x.join()
+        linkAdd = str(paragraphs[i - 1])
+        if not linkAdd.startswith('#'):
+            if not linkAdd.startswith('http'):
+                linkAdd = url + linkAdd
+            linkAdd1 = linkAdd.replace("https://", "")
+            linkAdd1 = linkAdd1.replace("http://", "")
+            if linkAdd1.endswith("/"):
+                linkAdd1 = linkAdd1[:-1]
+            if urlSite1.endswith("/"):
+                urlSite1 = urlSite1[:-1]
 
-            else:
-                l = url + h
-                l1 = l.replace("https://", "")
-                l2 = str(l1.replace("http://", ""))
-                g.edge(url2, l2)
-                if level < 3:
-                    n += 1
-                    x = threading.Thread(target=parseLinks, args=(g, l, level,))
-                    x.start()
-                    x.join()
+            g.edge(urlSite1, linkAdd1)
+
+            if (level < 3) and not (urlSite1 == linkAdd1):
+                if (level == 2) and not (urlSite1 in ng):
+                    continue
+                if linkAdd.endswith("/"):
+                    linkAdd = linkAdd[:-1]
+                if urlSite.endswith("/"):
+                    urlSite = urlSite[:-1]
+
+                x = threading.Thread(target=parseLinks, args=(ng1, g, linkAdd, level, urlSite1,))
+                n += 1
+                x.start()
+                threads.append(x)
+
+    for i in range(len(threads)):
+        threads[i].join()
 
     if n > 0:
-        logger.debug(url2 + " " + str(n))
+        print("n: " + str(n))
+        logger.debug(urlSite1)
 
 
-def matmult(v, G, b, e):
+# multiply matrix
+def matrixMultiply(vector, G, b, eVector):
     result = []
-    for i in range(len(G)):
+    for i1 in range(len(G)):
         total = 0
-        for j in range(len(G)):
-            total += v[j] * G[i][j]
-        result.append((b * total) + e)
+        for j1 in range(len(G)):
+            total += vector[j1] * G[i1][j1]
+        result.append((b * total) + eVector)
     return result
 
 
+def second(g):
+    g.view()
 
+    # parse logs
+    handle1 = open("threading.log", "r")
+    lineLog = []
+    for line in handle1:
+        if line:
+            lineLog.append(line.split("\n")[0].split(" ")[0])
+    handle1.close()
+
+    # add number for link
+    forNumber = {}
+    g = 0
+    for i in lineLog:
+        forNumber[i] = g
+        g += 1
+
+    # add 0 in matrix
+    matrix = []
+    for i in range(len(lineLog)):
+        matrix.append([])
+        for i1 in range(len(lineLog)):
+            matrix[i].append(0)
+
+    # parse connection between links
+    handle = open("second.gv", "r")
+    data = handle.readline()
+    for line in handle:
+        line1 = line.split("\n")[0]
+        line1 = line1.replace('}', '')
+
+        if line1:
+            line_first = line1.split(" ")[0].replace('\t', '').replace('"', '')
+            print("line_first " + line_first)
+            print("line1 " + line1)
+            line_second = line1.split(" ")[2].replace('\t', '').replace('"', '')
+
+            # add to matrix
+            if (line_first in forNumber.keys()) and (line_second in forNumber.keys()):
+                n1 = forNumber[line_first]
+                n2 = forNumber[line_second]
+                matrix[n2 - 1][n1 - 1] = matrix[n2 - 1][n1 - 1] + 1
+    handle.close()
+
+    for i3 in range(len(matrix)):
+        for j3 in range(len(matrix[i])):
+            print(str(matrix[i3][j3]), end=' ')
+        print()
+
+    v = []
+    for i in range(len(matrix)):
+        v.append(float(1 / len(matrix)))
+
+    for i1 in range(len(matrix)):
+        t = 0
+        for j in range(len(matrix[i1])):
+            t += matrix[j][i1]
+        print("t: " + str(t))
+        for j in range(len(matrix[i1])):
+            if (matrix[j][i1] > 0):
+                matrix[j][i1] = matrix[j][i1] / t
+
+    for i3 in range(len(matrix)):
+        for j3 in range(len(matrix[i])):
+            print(str(matrix[i3][j3]), end=' ')
+        print()
+
+    print("111")
+    e = float(1 - 0.85) / len(matrix)
+    v1 = matrixMultiply(v, matrix, 0.85, e)
+    v2 = matrixMultiply(v1, matrix, 0.85, e)
+    v3 = matrixMultiply(v2, matrix, 0.85, e)
+    v4 = matrixMultiply(v3, matrix, 0.85, e)
+    v5 = matrixMultiply(v4, matrix, 0.85, e)
+
+    tot = 0
+    for i1 in range(len(v5)):
+        tot += v5[i1]
+        print(str(v5[i1]))
+
+    print("result: " + str(tot))
+
+
+# parse links, get links
+ng2 = []
 g = Digraph('G', filename='second.gv')
 f = open('threading.log', 'w+')
 f.seek(0)
 f.close()
 logger = get_logger()
-url = 'https://m.vk.com/wrustem'
-parseLinks(g, url, 0)
-g.view()
-
-
-handle1 = open("threading.log", "r")
-lineLog = []
-for line in handle1:
-    if line:
-        lineLog.append(line)
-handle1.close()
-
-
-links1 = []
-for i in lineLog:
-    links1.append(i.split("\n")[0].split(" ")[0])
-    print(i)
-    print(i.split("\n")[0].split(" ")[1])
-
-forNumber = {}
-g = 0
-for i in links1:
-    forNumber[i] = [g, lineLog[g].split("\n")[0].split(" ")[1]]
-    g += 1
-
-print("length: " + str(len(forNumber)))
-matrix = []
-for i in range(len(links1)):
-    matrix.append([])
-    for i1 in range(len(links1)):
-        matrix[i].append(0)
-
-
-handle = open("second.gv", "r")
-data = handle.readline()
-lineLog = []
-for line in handle:
-        line1 = line.split("\n")[0]
-        line2 = line1.split(" ")[0]
-
-        line3 = line1.replace('}', '')
-        if line3:
-            line11 = line3.split(" ")[0]
-            line21 = line3.split(" ")[2]
-
-            l1 = line11.replace('\t', '')  # первый линк
-            l2 = line21.replace('\t', '')  # второй линк
-            l19 = l1.replace('"', '')  # первый линк
-            l29 = l2.replace('"', '')  # второй линк
-
-
-            print("line: " + line)
-            print("l19: " + l19)
-            print("l29: " + l29)
-
-            if (l19 in forNumber.keys()) and (l29 in forNumber.keys()):
-                n1 = forNumber[l19][0]
-                n3 = forNumber[l19][1]
-                n2 = forNumber[l29][0]
-                matrix[n1 - 1][n2 - 1] = matrix[n1 - 1][n2 - 1] + float(1 / int(n3))
-
-
-handle.close()
-
-
-
-v = []
-for i in range(len(matrix)):
-    v.append(float(1/len(matrix)))
-
-
-print("11111")
-for i1 in range(len(matrix)):
-    t = 0
-    for j in range(len(matrix[i1])):
-        t += matrix[i1][j]
-    print(t)
-
-e = float(1 - 0.85) / len(matrix)
-
-v1 = matmult(v, matrix, 0.85, e)
-v2 = matmult(v1, matrix, 0.85, e)
-v3 = matmult(v2, matrix, 0.85, e)
-v4 = matmult(v3, matrix, 0.85, e)
-v5 = matmult(v3, matrix, 0.85, e)
-#
-tot = 0
-for i1 in range(len(v4)):
-    tot += v4[i1]
-
-print(tot)
-
-
-
-
-
+url = 'https://www.google.com'
+parseLinks(ng2, g, url, 0, "")
+n = 0
+second(g)
